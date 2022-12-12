07689a6f73af43f98d0cd8f133c151c57a7e9740
CrmsWav <marcustritant@gmail.com>
2022-12-12 19:00:41 +0100

    Add Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import Layout from '../components/Layout'
+
 export default function Home() {
   return (
     <div>
-      <h1 className='text-3xl font-bold'>YAMS</h1>
+      <Layout />
     </div>
   )
 }
