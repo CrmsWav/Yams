9705e57ac71ef7bc2b3438914bb454ef8421f3fd
CrmsWav <marcustritant@gmail.com>
2022-12-16 12:06:59 +0100

    UI

--- pages/prizes/index.tsx
+++ pages/prizes/index.tsx
@@ -5,13 +5,19 @@ import Layout from '../../components/Layout';
 function Prizes({ datas }: any) {  
   return (
     <Layout>
-      <div className='flex flex-col items-center'>
-          <h1 className='text-3xl font-bold'>Prizes</h1>
+      <div className='h-full flex flex-col items-center justify-between'>
+          <h1 className='text-3xl font-bold my-4 text-tertiary'>Prizes</h1>
 
-          <ul className=''>
+          <h2 className='text-2xl font-bold my-4'>To Won</h2>
+          <ul>
             {datas[0].pastries.map((pastrie: any, id: any) => (
               <li key={id}>{pastrie.name}</li>
             ))}
+          </ul>
+
+          <h2 className='text-2xl font-bold my-4'>Won</h2>
+          <ul>
+            <li>Cake aux fruits (A/M/J/H/S)</li>
           </ul>
       </div>
     </Layout>
